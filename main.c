e8dca0e60aeb2f67eee036ac2b7b2e20ebb80d28
Ivo Georgiev <ivogeorg@gmail.com>
2016-02-15 17:12:42 -0700

    Minor update

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "mem_pool.h"
 
 int main(int argc, char *argv[]) {
 
     printf("Testing C Language Programming Assignment\n");
+
+    printf("%ld\n", __STDC_VERSION__);
+
+    const unsigned POOL_SIZE = 1000000;
+    pool_pt pool = NULL;
+
+    alloc_status status = mem_init();
+    assert(status == ALLOC_OK);
+    pool = mem_pool_open(POOL_SIZE, FIRST_FIT); // TODO add policy to pool
+    assert(pool);
+
+    status = mem_pool_close(pool);
+    assert(status == ALLOC_OK);
+    status = mem_free();
+    printf("status = %d\n", status);
+    assert(status == ALLOC_OK);
 
     return 0;
 }
--- mem_pool.h
+++ mem_pool.h
@@ -9,8 +9,11 @@
 
 /* type declarations */
 
+typedef enum _alloc_policy { FIRST_FIT, BEST_FIT } alloc_policy;
+
 typedef struct _pool {
-    char *pool;
+    char *mem;
+    alloc_policy policy;
     size_t total_size;
     size_t alloc_size;
     unsigned num_allocs;
@@ -22,20 +25,33 @@ typedef struct _alloc {
     char *mem;
 } alloc_t, *alloc_pt;
 
-typedef enum _alloc_status { ALLOC_OK, ALLOC_FAIL } alloc_status;
-
-typedef enum _alloc_policy { FIRST_FIT, BEST_FIT } alloc_policy;
+typedef enum _alloc_status {
+    ALLOC_OK,
+    ALLOC_FAIL,
+    ALL0C_CALLED_AGAIN,
+    ALLOC_NOT_FREED
+} alloc_status;
 
 /* function declarations */
 
 /* TODO descriptions */
-pool_pt mem_pool_open(size_t size, alloc_policy policy);
+alloc_status
+mem_init();
 
-alloc_status mem_pool_close(pool_pt pool);
+alloc_status
+mem_free();
 
-alloc_pt mem_new_alloc(pool_pt pool, size_t size);
+pool_pt
+mem_pool_open(size_t size, alloc_policy policy);
 
-alloc_status mem_del_alloc(pool_pt pool, alloc_pt alloc);
+alloc_status
+mem_pool_close(pool_pt pool);
+
+alloc_pt
+mem_new_alloc(pool_pt pool, size_t size);
+
+alloc_status
+mem_del_alloc(pool_pt pool, alloc_pt alloc);
 
 /* TODO printing functions for debugging & testing */
 
