2508ab658565ac7300473cbad475e1640cc69279
Ivo Georgiev <ivogeorg@gmail.com>
2016-02-20 11:25:30 -0700

    Fixed signature bug in mem_inspect_pool

--- mem_pool.h
+++ mem_pool.h
@@ -39,7 +39,6 @@ typedef enum _alloc_status {
 
 /* function declarations */
 
-/* TODO descriptions */
 alloc_status
 mem_init();
 
@@ -59,6 +58,6 @@ alloc_status
 mem_del_alloc(pool_pt pool, alloc_pt alloc);
 
 void
-mem_inspect_pool(pool_pt pool, pool_segment_pt segments, unsigned *num_segments);
+mem_inspect_pool(pool_pt pool, pool_segment_pt *segments, unsigned *num_segments);Trivial
 
 #endif //DENVER_OS_PA_C_MEM_POOL_H
